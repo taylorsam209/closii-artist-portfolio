1141fb6b84d19a3f7c6214330d5365ed4fbbcc30
Taylor Sam <taylorsam209@yahoo.com>
2018-01-09 19:15:55 -0800

    Completed mobile view prototype, Stripe implementation test phase

--- server/server.js
+++ server/server.js
@@ -2,11 +2,29 @@ require('dotenv').config();
 const express = require("express"),
     bodyParser = require("body-parser"),
     cors = require('cors'),
-    axios = require('axios')
+    axios = require('axios'),
+    exphbs = require("express-handlebars"),
+    stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-    const port = 3020;
-    const app = express();
-    app.use(bodyParser.json());
-    app.use(cors());
+const port = process.env.PORT;
+const app = express();
+app.use(cors());
 
-    app.listen(port, ()=> console.log(`Running on port ${port}`))
+//Handlebars middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+
+//Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//Set Static Folder
+app.use(express.static(`${__dirname}/public`))
+
+//Index Route
+app.get('/', (req, res) => {
+    res.render("index");
+});
+
+app.listen(port, () => console.log(`Running on port ${port}`))
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
 import './Landing.css';
+import music from '../assets/headOverHeels.mp3';
+import 'font-awesome/css/font-awesome.min.css';
 
 
 class Landing extends Component {
     constructor() {
         super();
         this.state = {
-            audio: []
+            audio: ["Ooh Baby Baby", "Let's Dance under the Moonlight", "Paparazzi"]
         }
+
+        this.handleAudio = this.handleAudio.bind(this);
+    }
+
+    handleAudio() {
+        return this.state.audio.map((e, i, arr) => {
+            return <div key={i}>Song: {e}</div>
+        })
+
     }
     render() {
         return (
             <div id="Landing">
                 <header id="banner" className='grid'>
                     <div className="bg-image">
-                        Closii
-          </div>
+                        <h1>Closii</h1>
+                        <h2>Artist of the Mind</h2>
+                    </div>
                 </header>
                 <main>
                     <section id="section-bio" className="grid">
@@ -31,16 +43,40 @@ class Landing extends Component {
                                 qui officia deserunt mollit anim id est laborum.</p>
                     </section>
                     <section id="section-music" className="grid">
-                    <h2>Music</h2>
-                    <h3>Song, Artist, Info</h3>
-                    
+                        <h2>Music</h2>
 
+                        {this.handleAudio()}
+
+                        <h3>Song, Artist, Info...Free Download! Click on the download icon on the control bar.</h3>
+                        <audio src={music} type="audio/mp3" controls />
+
+                        <h3>Song, Artist, Info</h3>
+                        <audio src={music} type="audio/mp3" controls controlsList="nodownload" />
+
+
+                        <h3>Song, Artist, Info</h3>
+                        <audio src={music} type="audio/mp3" controls controlsList="nodownload" />
+
+                        <h3>Song, Artist, Info</h3>
+                        <audio style={{ marginBottom: '20px' }} src={music} type="audio/mp3" controls controlsList="nodownload" />
+                    </section>
+                    <section id="section-contact" className="grid">
+                        <h2>Reach Out and Connect</h2>
+                        <p>closii@musicislife.com</p>
+                        <a className="instagram" href="https://www.instagram.com/closprez/" target="_blank" rel="noopener noreferrer">
+                            <i class="fa fa-instagram fa-2x" aria-hidden="true"></i> closprez
+                            </a>
+                        <h3>Would you like to support?</h3>
+                        <div className="donate-btn"><a href="#" >Donate!</a></div>
                     </section>
 
                 </main>
 
-                <footer>
-
+                <footer id="main-footer" class="grid">
+                    <div>CLOSII Music, inc.</div>
+                    <div>Project By
+                        <a href="http://taylorsam209.wixsite.com/home" target="_blank" rel="noopener noreferrer"> Saylor Designs</a>
+                    </div>
                 </footer>
             </div>
         );
