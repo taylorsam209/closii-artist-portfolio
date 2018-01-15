fbac55f9dad02bd975b91bae3812a28b0a06fbe8
Taylor Sam <taylorsam209@yahoo.com>
2018-01-14 19:12:42 -0800

    Added video section

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './Landing.css';
-import music from '../assets/headOverHeels.mp3';
+import musicFile from '../assets/headOverHeels.mp3';
 import 'font-awesome/css/font-awesome.min.css';
 import Checkout from './Checkout';
 import bicycle from '../assets/bicycle.JPG';
@@ -8,14 +8,27 @@ import flowerOfLife from '../assets/flowerOfLife.JPG';
 import classroom from '../assets/classroom.JPG';
 import stump from '../assets/stump.JPG';
 import lightSpeed from '../assets/lightSpeed.JPG';
+import intro from '../assets/intro.jpg';
+import video from '../assets/video.mp4';
 
 class Landing extends Component {
     constructor() {
         super();
+        var songObj = {
+            name: "Head Over Heels",
+            src: '../assets/headOverHeels.mp3'
+        };
 
         this.state = {
             donationAmount: '',
-            audio: ["Ooh Baby Baby", "Let's Dance under the Moonlight", "Paparazzi"]
+            audio: ["Ooh Baby Baby", "Let's Dance under the Moonlight", "Paparazzi"],
+            playlist: [
+                songObj = {
+                    name: "Head Over Heels",
+                    src: musicFile,
+                    img: classroom
+                }
+            ]
         }
 
         this.handleAudio = this.handleAudio.bind(this);
@@ -42,42 +55,44 @@ class Landing extends Component {
                 </header>
                 <main>
                     <section id="section-bio" className="grid">
-                        <img className="bio-image" src={classroom} alt="Carlos Perez" />
+                    <h1>ClosII Music</h1>
+                        <img className="bio-image" src={intro} alt="Carlos Perez" />
                         <h2>Bio</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur
-                                adipiscing elit, sed do eiusmod tempor incididunt
-                                ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                 Duis aute irure dolor in reprehenderit in voluptate velit
-                                  esse cillum dolore eu fugiat nulla pariatur.
-                                  Excepteur sint occaecat cupidatat non proident, sunt in culpa
-                                qui officia deserunt mollit anim id est laborum.</p>
+                        <p>Please give me money for new shoes. Lorem Ipsum etc erefdaofsdfosafmdof dos fadfaso
+                            fdsjofs ioajfs ioanfmsio fmiasmfiasnmofo o ofdmsaoifms ofmajw9nfo dsalkm oim\avpmv
+                             vsavcpaksuvj auvnizjnv knaij valuevvao vmosmo ij9uvawr9gnawo ndvskvcosnoino
+                             fiajfdj9wjfoisa jfoiawgfihwoug owjfowiakfoj dwojadfwj90jvoi oadfmsoi afidjowfion .
+                        </p>
                     </section>
 
                     <section id="section-music" className="grid">
                         <h2>Music</h2>
 
-                        {this.handleAudio()}
-
-                        <h3>Song, Artist, Info...Free Download! Click on the download icon on the control bar.</h3>
-                        <audio src={music} type="audio/mp3" controls />
+                        <h3>Free Download!</h3>
+                        <audio src={musicFile} type="audio/mp3" controls />
 
                         <h3>Song, Artist, Info</h3>
-                        <audio src={music} type="audio/mp3" controls controlsList="nodownload" />
+                        <audio src={musicFile} type="audio/mp3" controls controlsList="nodownload" />
 
 
                         <h3>Song, Artist, Info</h3>
-                        <audio src={music} type="audio/mp3" controls controlsList="nodownload" />
+                        <audio src={musicFile} type="audio/mp3" controls controlsList="nodownload" />
 
                         <h3>Song, Artist, Info</h3>
-                        <audio style={{ marginBottom: '20px' }} src={music} type="audio/mp3" controls controlsList="nodownload" />
+                        <audio style={{ marginBottom: '20px' }} src={musicFile} type="audio/mp3" controls controlsList="nodownload" />
+                    </section>
+                    <section id="section-video" className='grid'>
+                    <h2>Videos</h2>
+                    <video width="80%" height="auto" controls controlsList="nodownload">
+                        <source src={video} type='video/mp4'/>
+                    </video>
                     </section>
 
                     <section id="section-contact" className="grid">
                         <h2>Reach Out and Connect</h2>
                         <p>closii@musicislife.com</p>
                         <a className="instagram" href="https://www.instagram.com/closprez/" target="_blank" rel="noopener noreferrer">
-                            <i className="fa fa-instagram fa-2x" aria-hidden="true"></i> closprez
+                            <i className="fa fa-instagram fa-2x" aria-hidden="true"></i> Follow me on Instagram at closprez
                             </a>
                         <h3>Would you like to support? Make a donation!</h3>
                         <h4>Help fund the cost of hosting music, equipment, and live shows.</h4>
@@ -106,3 +121,4 @@ class Landing extends Component {
 }
 
 export default Landing;
+
