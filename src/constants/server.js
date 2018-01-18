f2f134baf28427d6f38e50b19ed88ad012c3cddf
Taylor Sam <taylorsam209@yahoo.com>
2018-01-17 18:08:44 -0800

    Reconfigured Server lines

--- src/constants/server.js
+++ src/constants/server.js
@@ -1,5 +1,5 @@
 const PAYMENT_SERVER_URL = process.env.NODE_ENV === 'production'
-  ? 'http://myapidomain.com'
+  ? 'http://closiimusic.com'
   : `http://localhost:${process.env.REACT_APP_PORT}`;
 
 export default PAYMENT_SERVER_URL;
